735ab384b2713ada12117607a14456fb4dd019ef
remoo1901 <remoo1901@hotmail.com>
2020-04-30 19:48:29 -0400

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
-import ProductContext from "./contexts/ProductContext"
-import CartContext from "./contexts/CartContext"
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-		setCart([...cart, item])
-	};
+  const addItem = (item) => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-		   <ProductContext.Provider value={{products, addItem}} >
-		   <CartContext.Provider cart={cart}>
-			<Navigation />
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider cart={cart}>
+          <Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products />
-			</Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-			<Route path="/cart">
-				<ShoppingCart />
-			</Route>
-			</CartContext.Provider>
-			</ProductContext.Provider>
-
-		</div>
-	);
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
